aa8891fbf7ba805db9fc94840a44bf00ccc95954
Sam Hocevar <sam@hocevar.net>
2022-04-27 16:05:58 +0200

    Start adding the Joystick API, with stubs for the browser

--- desktop.go
+++ desktop.go
@@ -342,6 +342,65 @@ const (
 	MouseButtonMiddle = MouseButton(glfw.MouseButtonMiddle)
 )
 
+type Joystick glfw.Joystick
+
+const (
+	Joystick1    = Joystick(glfw.Joystick1)
+	Joystick2    = Joystick(glfw.Joystick2)
+	Joystick3    = Joystick(glfw.Joystick3)
+	Joystick4    = Joystick(glfw.Joystick4)
+	Joystick5    = Joystick(glfw.Joystick5)
+	Joystick6    = Joystick(glfw.Joystick6)
+	Joystick7    = Joystick(glfw.Joystick7)
+	Joystick8    = Joystick(glfw.Joystick8)
+	Joystick9    = Joystick(glfw.Joystick9)
+	Joystick10   = Joystick(glfw.Joystick10)
+	Joystick11   = Joystick(glfw.Joystick11)
+	Joystick12   = Joystick(glfw.Joystick12)
+	Joystick13   = Joystick(glfw.Joystick13)
+	Joystick14   = Joystick(glfw.Joystick14)
+	Joystick15   = Joystick(glfw.Joystick15)
+	Joystick16   = Joystick(glfw.Joystick16)
+	JoystickLast = Joystick(glfw.JoystickLast)
+)
+
+type GamepadAxis glfw.GamepadAxis
+
+const (
+	AxisLeftX        = GamepadAxis(glfw.AxisLeftX)
+	AxisLeftY        = GamepadAxis(glfw.AxisLeftY)
+	AxisRightX       = GamepadAxis(glfw.AxisRightX)
+	AxisRightY       = GamepadAxis(glfw.AxisRightY)
+	AxisLeftTrigger  = GamepadAxis(glfw.AxisLeftTrigger)
+	AxisRightTrigger = GamepadAxis(glfw.AxisRightTrigger)
+	AxisLast         = GamepadAxis(glfw.AxisLast)
+)
+
+type GamepadButton glfw.GamepadButton
+
+const (
+	ButtonA           = GamepadButton(glfw.ButtonA)
+	ButtonB           = GamepadButton(glfw.ButtonB)
+	ButtonX           = GamepadButton(glfw.ButtonX)
+	ButtonY           = GamepadButton(glfw.ButtonY)
+	ButtonLeftBumper  = GamepadButton(glfw.ButtonLeftBumper)
+	ButtonRightBumper = GamepadButton(glfw.ButtonRightBumper)
+	ButtonBack        = GamepadButton(glfw.ButtonBack)
+	ButtonStart       = GamepadButton(glfw.ButtonStart)
+	ButtonGuide       = GamepadButton(glfw.ButtonGuide)
+	ButtonLeftThumb   = GamepadButton(glfw.ButtonLeftThumb)
+	ButtonRightThumb  = GamepadButton(glfw.ButtonRightThumb)
+	ButtonDpadUp      = GamepadButton(glfw.ButtonDpadUp)
+	ButtonDpadRight   = GamepadButton(glfw.ButtonDpadRight)
+	ButtonDpadDown    = GamepadButton(glfw.ButtonDpadDown)
+	ButtonDpadLeft    = GamepadButton(glfw.ButtonDpadLeft)
+	ButtonLast        = GamepadButton(glfw.ButtonLast)
+	ButtonCross       = GamepadButton(glfw.ButtonCross)
+	ButtonCircle      = GamepadButton(glfw.ButtonCircle)
+	ButtonSquare      = GamepadButton(glfw.ButtonSquare)
+	ButtonTriangle    = GamepadButton(glfw.ButtonTriangle)
+)
+
 type Action glfw.Action
 
 const (
@@ -374,6 +433,27 @@ const (
 	ModAlt     = ModifierKey(glfw.ModAlt)
 	ModSuper   = ModifierKey(glfw.ModSuper)
 )
+
+func (joy Joystick) IsPresent() bool {
+	return glfw.Joystick(joy).Present()
+}
+
+func (joy Joystick) GetGamepadName() string {
+	return glfw.Joystick(joy).GetGamepadName()
+}
+
+func (joy Joystick) GetAxes() []float32 {
+	return glfw.Joystick(joy).GetAxes()
+}
+
+func (joy Joystick) GetButtons() []Action {
+	src := glfw.Joystick(joy).GetButtons()
+	dst := make([]Action, len(src))
+	for i, v := range src {
+		dst[i] = Action(v)
+	}
+	return dst
+}
 
 // Open opens a named asset. It's the caller's responsibility to close it when done.
 //
